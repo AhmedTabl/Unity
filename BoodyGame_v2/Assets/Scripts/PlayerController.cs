c0409d6f6ffa5cda1ae44cf2f5f3c2125f523de3
AhmedTabl <ahmedmegahed20142@gmail.com>
2020-04-08 06:16:36 -0400

    running animation

--- BoodyGame_v2/Assets/Scripts/PlayerController.cs
+++ BoodyGame_v2/Assets/Scripts/PlayerController.cs
@@ -6,25 +6,34 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rigidbody;
 
-    
+    public Animator anime;
 
 
  
    private void Update()
     {
 
-        if (Input.GetKey(KeyCode.A)) {
+        if (Input.GetKey(KeyCode.A))
+        {
 
             rigidbody.velocity = new Vector2(-8, rigidbody.velocity.y);
             transform.localScale = new Vector3(-1, 1, 1);
+            anime.SetBool("running", true);
+
         }
-        if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
 
             rigidbody.velocity = new Vector2(8, rigidbody.velocity.y);
             transform.localScale = new Vector3(1, 1, 1);
+            anime.SetBool("running", true);
 
         }
+        else {
+
+            anime.SetBool("running", false);
+        }
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
 
