30bbf756f7fc702762efd529249ab2855ad364cc
AhmedTabl <ahmedmegahed20142@gmail.com>
2020-04-14 00:24:21 -0400

    Added jumping and falling animations

--- BoodyGame_v2/Assets/Scripts/PlayerController.cs
+++ BoodyGame_v2/Assets/Scripts/PlayerController.cs
@@ -4,44 +4,83 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody2D rigidbody;
+    private Rigidbody2D rigidbody;
+    private Animator anime;
+    public Joystick joystick;
+    private enum State { idle, running, jumping , falling};
+    private Collider2D collider;
+    [SerializeField]private LayerMask ground;
+    private State state = State.idle;
 
-    public Animator anime;
+    public float speed = 20f;
+    public float jumpForce = 50f;
 
-
- 
-   private void Update()
+    private void Start()
     {
+        rigidbody = GetComponent<Rigidbody2D>();
+        anime = GetComponent<Animator>();
+        collider = GetComponent< CapsuleCollider2D>();
 
-        if (Input.GetKey(KeyCode.A))
+    }
+
+    private void Update()
+    {
+       float  horizontal_dirction = Input.GetAxis("Horizontal");
+       
+        if (horizontal_dirction < 0)
         {
 
-            rigidbody.velocity = new Vector2(-8, rigidbody.velocity.y);
-            transform.localScale = new Vector3(-1, 1, 1);
-            anime.SetBool("running", true);
+            rigidbody.velocity = new Vector2(-speed, rigidbody.velocity.y);
+            transform.localScale = new Vector3(-4, 4, 1);
 
         }
-        else if (Input.GetKey(KeyCode.D))
+        else if (horizontal_dirction >0)
         {
 
-            rigidbody.velocity = new Vector2(8, rigidbody.velocity.y);
-            transform.localScale = new Vector3(1, 1, 1);
-            anime.SetBool("running", true);
-
+            rigidbody.velocity = new Vector2(speed, rigidbody.velocity.y);
+            transform.localScale = new Vector3(4, 4, 1);
         }
         else {
 
-            anime.SetBool("running", false);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if ((Input.GetButtonDown("Jump")) && (collider.IsTouchingLayers(ground)))
         {
 
-            rigidbody.velocity = new Vector2(rigidbody.velocity.x, 23);
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpForce);
+            state = State.jumping;
 
         }
 
+        velocity_state();
+        anime.SetInteger("state", (int)state);
 
+    }
+    private void velocity_state() {
+        if (state == State.jumping)
+        {
+
+            if (rigidbody.velocity.y < 0.1f)
+            {
+                state = State.falling;
+            }
+        }
+        else if (state == State.falling)
+        {
+            if (collider.IsTouchingLayers(ground))
+            {
+                state = State.idle;
+            }
+        }
+        else if (Mathf.Abs(rigidbody.velocity.x) > 2f)
+        {
+            //Moving
+            state = State.running;
+        }
+        else {
+
+            state = State.idle;
+        }
 
     }
 }
