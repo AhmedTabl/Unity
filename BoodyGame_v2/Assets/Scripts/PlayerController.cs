8e1ecdcd9b7a4d8e843e232fe73e7b25c774c80d
AhmedTabl <ahmedmegahed20142@gmail.com>
2020-04-22 04:33:07 -0400

    Added frog enemy(with idle animation)
    You bounce off the frog when you hit it from the front or back. You kill the frog if you fall on it.

--- BoodyGame_v2/Assets/Scripts/PlayerController.cs
+++ BoodyGame_v2/Assets/Scripts/PlayerController.cs
@@ -12,64 +12,111 @@ public class PlayerController : MonoBehaviour
     [SerializeField]private Text CherryCounter;
 
     //Finite state machine
-    private enum State { idle, running, jumping , falling};
+    private enum State { idle, running, jumping , falling, hurt};
     private State state = State.idle;
 
     //Inspector variables
     [SerializeField] private LayerMask ground;
     [SerializeField] private float speed = 20f;
     [SerializeField] private float jumpForce = 50f;
-    [SerializeField]private int cherries = 0;
+    private int cherries = 0;
+    [SerializeField] private float HitForce = 50f;
 
+
+    //Main Functions
     private void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
         anime = GetComponent<Animator>();
-        collider = GetComponent< CapsuleCollider2D>();
+        collider = GetComponent<CapsuleCollider2D>();
 
     }
-
     private void Update()
     {
-        Movement();
+        if (state != State.hurt) {
+
+            Movement();
+        }
         Animation_State();
         anime.SetInteger("state", (int)state);
     }
 
+
+    //Collision and Collectables functions
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Collectable") {
+
 
             Destroy(collision.gameObject);
             cherries++;
             CherryCounter.text = cherries.ToString();
         }
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            if (state == State.falling) {
+                Destroy(collision.gameObject);
+                Jump();
+            }
+            else
+            {
+                state = State.hurt;
+                if (collision.transform.position.x > transform.position.x)
+                {
+                    //The enemy is to our right so we get bounced to the left
+                    rigidbody.velocity = new Vector2(-HitForce, rigidbody.velocity.y);
+
+                }
+                else
+                {
+                    //The enemy is to our left so we get boumced to the right
+                    rigidbody.velocity = new Vector2(HitForce, rigidbody.velocity.y);
+                }
+            }
+        }
+    }
+
+    //Movement and State functions
     private void Movement()
     {
         float horizontal_dirction = Input.GetAxis("Horizontal");
 
-        if (horizontal_dirction < 0)
+        if (horizontal_dirction < 0f)
         {
 
             rigidbody.velocity = new Vector2(-speed, rigidbody.velocity.y);
             transform.localScale = new Vector3(-4, 4, 1);
+            if (horizontal_dirction > -0.25f)
+            {
+                rigidbody.velocity = new Vector2(0, rigidbody.velocity.y);
+            }
 
         }
-        else if (horizontal_dirction > 0)
+        else if (horizontal_dirction > 0f)
         {
 
             rigidbody.velocity = new Vector2(speed, rigidbody.velocity.y);
             transform.localScale = new Vector3(4, 4, 1);
+            if (horizontal_dirction < 0.25f)
+            {
+                rigidbody.velocity = new Vector2(0, rigidbody.velocity.y);
+            }
         }
 
         if ((Input.GetButtonDown("Jump")) && (collider.IsTouchingLayers(ground)))
         {
 
-            rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpForce);
-            state = State.jumping;
+            Jump();
 
         }
+    }
+    private void Jump()
+    {
+        rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpForce);
+        state = State.jumping;
     }
     private void Animation_State() {
         if (state == State.jumping)
@@ -83,6 +130,14 @@ public class PlayerController : MonoBehaviour
         else if (state == State.falling)
         {
             if (collider.IsTouchingLayers(ground))
+            {
+                state = State.idle;
+            }
+        }
+        else if (state == State.hurt)
+        {
+
+            if (Mathf.Abs(rigidbody.velocity.x) < 0.1f)
             {
                 state = State.idle;
             }
