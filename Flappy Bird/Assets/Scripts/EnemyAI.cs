70d95748cd3787914290ae7b8f7dd30efa0bc9f6
AhmedTabl <ahmedmegahed20142@gmail.com>
2020-07-06 11:46:25 -0400

    Added enemy AI

---
+++ Flappy Bird/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField] private Transform player;
+    [SerializeField] private float speed = 5;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement();
+    }
+
+    private void movement()
+    {
+        if ((player.position.x  < transform.position.x) && (player.position.y < transform.position.y))
+        {
+            rb.velocity = new Vector2(-speed, -speed);
+        }
+        if ((player.position.x -3 > transform.position.x) && (player.position.y > transform.position.y))
+        {
+            transform.localScale = new Vector2(-2,2);
+            rb.velocity = new Vector2(speed, speed);
+        }
+        if ((player.position.x < transform.position.x) && (player.position.y > transform.position.y))
+        {
+            rb.velocity = new Vector2(-speed, speed);
+        }
+        if ((player.position.x -3 > transform.position.x) && (player.position.y < transform.position.y))
+        {
+            transform.localScale = new Vector2(-2, 2);
+            rb.velocity = new Vector2(speed, -speed);
+        }
+    }
+
+}
