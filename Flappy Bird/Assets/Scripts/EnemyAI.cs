8b4419b1cc038fae130795d0babecf5e653ea861
AhmedTabl <ahmedmegahed20142@gmail.com>
2020-07-09 10:38:18 -0400

    Expanded levels 1 and 2

--- Flappy Bird/Assets/Scripts/EnemyAI.cs
+++ Flappy Bird/Assets/Scripts/EnemyAI.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class EnemyAI : MonoBehaviour
 {
     private Rigidbody2D rb;
+
+    [SerializeField] private float responseDelay;
+    [SerializeField] private float rangeOfVision;
     [SerializeField] private Transform player;
     [SerializeField] private float speed = 5;
     
@@ -23,23 +26,39 @@ public class EnemyAI : MonoBehaviour
 
     private void movement()
     {
-        if ((player.position.x  < transform.position.x) && (player.position.y < transform.position.y))
+        if (((player.position.x -responseDelay < transform.position.x) && (player.position.y < transform.position.y)) && (transform.position.x - player.transform.position.x < rangeOfVision))
         {
             rb.velocity = new Vector2(-speed, -speed);
         }
-        if ((player.position.x -3 > transform.position.x) && (player.position.y > transform.position.y))
+        else if (((player.position.x  > transform.position.x) && (player.position.y > transform.position.y)) && (transform.position.x - player.transform.position.x < rangeOfVision))
         {
             transform.localScale = new Vector2(-2,2);
             rb.velocity = new Vector2(speed, speed);
         }
-        if ((player.position.x < transform.position.x) && (player.position.y > transform.position.y))
+        else if (((player.position.x -responseDelay < transform.position.x) && (player.position.y > transform.position.y)) && (transform.position.x - player.transform.position.x < rangeOfVision))
         {
             rb.velocity = new Vector2(-speed, speed);
         }
-        if ((player.position.x -3 > transform.position.x) && (player.position.y < transform.position.y))
+        else if (((player.position.x  > transform.position.x) && (player.position.y < transform.position.y)) && (transform.position.x - player.transform.position.x < rangeOfVision))
         {
             transform.localScale = new Vector2(-2, 2);
             rb.velocity = new Vector2(speed, -speed);
+        }
+        else if (((player.position.x == transform.position.x) && (player.position.y < transform.position.y)) && (transform.position.x - player.transform.position.x < rangeOfVision))
+        {
+            rb.velocity = new Vector2(0, -speed);
+        }
+        else if (((player.position.x == transform.position.x) && (player.position.y > transform.position.y)) && (transform.position.x - player.transform.position.x < rangeOfVision))
+        {
+            rb.velocity = new Vector2(0, speed);
+        }
+        else if (((player.position.x -responseDelay < transform.position.x) && (player.position.y == transform.position.y)) && (transform.position.x - player.transform.position.x < rangeOfVision))
+        {
+            rb.velocity = new Vector2(-speed, 0);
+        }
+        else if (((player.position.x > transform.position.x) && (player.position.y == transform.position.y)) && (transform.position.x - player.transform.position.x < rangeOfVision))
+        {
+            rb.velocity = new Vector2(speed, 0);
         }
     }
 
