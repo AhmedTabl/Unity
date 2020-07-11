414b14a3c4cd9c5eb569cf3fc1d0cd8617d27873
AhmedTabl <ahmedmegahed20142@gmail.com>
2020-07-11 11:50:53 -0400

    Revert "Edited the entire game's speed to make it more fast paced"
    This reverts commit e123e0b872603a9fb99200c2d31aa1d95ef08d54.

--- Flappy Bird/Assets/Scripts/ObstacleMover.cs
+++ Flappy Bird/Assets/Scripts/ObstacleMover.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class ObstacleMover : MonoBehaviour
 {
-    [SerializeField] private bool upToDown;
-    [SerializeField] private bool downToUp;
     [SerializeField] private float speed = 0;
 
     private Rigidbody2D rb;
@@ -14,28 +12,17 @@ public class ObstacleMover : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
-        if (upToDown == true)
-        {
-            downToUp = false;
-            rb.velocity = new Vector2(0, -speed);
-        }
-        if (downToUp == true)
-        {
-            upToDown = false;
-            rb.velocity = new Vector2(0, speed);
-        }
+        rb.velocity = new Vector2(0, speed);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.y >= 39)
+        if(transform.position.y >= 35)
         {
             rb.velocity = new Vector2(0, -speed);
 
-        }
-        if (transform.position.y <= 3)
+        }else if (transform.position.y <= 2)
         {
             rb.velocity = new Vector2(0, speed);
         }
