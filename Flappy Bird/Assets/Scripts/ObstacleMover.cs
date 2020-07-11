3a08eea426a8029f41d12b70a510f9f37d6183ef
AhmedTabl <ahmedmegahed20142@gmail.com>
2020-07-11 01:35:29 -0400

    added level 3 and a new moving obstacle

---
+++ Flappy Bird/Assets/Scripts/ObstacleMover.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMover : MonoBehaviour
+{
+    [SerializeField] private float speed = 0;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0, speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y >= 35)
+        {
+            rb.velocity = new Vector2(0, -speed);
+
+        }else if (transform.position.y <= 2)
+        {
+            rb.velocity = new Vector2(0, speed);
+        }
+
+
+    }
+}
